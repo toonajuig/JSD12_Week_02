09c13c08795dbcff875fe18a6d59e78cc17caf1d
toonajuig <thirawat.devtt@gmail.com>
2026-03-10 15:03:10 +0700

    added bring-it-to-js

--- 06_bring-it-to-javascript.js
+++ 06_bring-it-to-javascript.js
@@ -1,1 +1,54 @@
-a
+// ProductList Evosl and Pro4
+let productList =["adidas_evo_001", "adidas_evo_002", "adidas_pro4_001", "adidas_pro4_002",]
+
+// Product Data
+const products_A ={
+    productId: "adidas_evo_001",
+    productName: "Adidas EvoSL White",
+    price: 5800
+  }
+
+  const products_B ={
+    productId: "adidas_evo_002",
+    productName: "Adidas EvoSL Black",
+    price: 5800
+  }
+ const products_C = {
+    productId: "adidas_pro4_001",
+    productName: "Adidas Adios Pro 4 White",
+    price: 9500
+  }
+ const products_D = {
+    productId: "adidas_pro4_002",
+    productName: "Adidas Adios Pro 4 Pink",
+    price: 9500
+  }
+
+//Cart Data
+let cart = [
+    {
+        productId: "adidas_evo_001",
+        quantity: 9
+    },
+    {
+        productId: "adidas_evo_002",
+        quantity: 8
+    },
+    {
+        productId: "adidas_pro4_001",
+        quantity: 7
+    },
+    {
+        productId: "adidas_pro4_002",
+        quantity: 6
+    },
+];
+
+// Consolelog
+console.log("Product List: ", productList);
+console.log("ProductA: ",products_A);
+console.log("Cart: ",cart);
+
+
+
+
